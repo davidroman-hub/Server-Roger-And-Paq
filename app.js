d20a2992140d8e8a7c9161912339a81ffc672cf8
unknown <jobroman83@gmail.com>
2019-12-30 17:41:11 +0100

    adding the first route

--- app.js
+++ app.js
@@ -16,12 +16,23 @@ mongoose.connect(process.env.DATABASE,{
 })
 .then(()=>console.log('Db Connected'))
 
+//import routes
+const userRoutes = require('./Routes/user')
+
+
 //routes 
 app.get('/', (req,res)=>{
 
     res.send('hello from node ;)')
 
 });
+
+app.use('/api', userRoutes)
+
+
+
+
+//server
 
 const port = process.env.PORT || 8000
 
