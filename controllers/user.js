5f9fcacd2a4db75b85e0c919ec4a2cdbb64088a2
unknown <jobroman83@gmail.com>
2019-12-30 19:13:08 +0100

    adding error handler

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const {errorHandler} = require('../helpers/dbErrorHandle')
 
 exports.signup = (req, res) => {
     
@@ -6,7 +7,7 @@ exports.signup = (req, res) => {
     user.save((err,user) =>{
         if(err){
             return res.status(400).json({
-                err
+                err: errorHandler(err)
             });
         }
         res.json({
