10f067703c317721dae721bb498fb098362ac852
unknown <jobroman83@gmail.com>
2019-12-30 19:16:17 +0100

    undefined salt and password

--- controllers/user.js
+++ controllers/user.js
@@ -10,6 +10,8 @@ exports.signup = (req, res) => {
                 err: errorHandler(err)
             });
         }
+        user.salt = undefined
+        user.hashed_password = undefined
         res.json({
             user
         })
