a9f2f6990ae06c93d72af0a236f178bd5c873fc3
davidroman-hub <jobroman83@gmail.com>
2020-01-08 19:24:45 +0100

    onle admin its able to create Category

--- Routes/category.js
+++ Routes/category.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router()
 
-const{
+const { create } = require('../controllers/category');
+const { requireSignin, isAuth,isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 
-    create
 
-} = require('../controllers/category');
 
-router.post('/category/create', create)
+
+router.post('/category/create/:userId',requireSignin,isAdmin,isAuth, create)
+
+router.param("userId", userById)
 
 module.exports = router;
