1bdb651c57c2ad481b01ddeeaea0d3f9499a512e
davidroman-hub <jobroman83@gmail.com>
2020-01-09 15:45:18 +0100

    Delete Product

--- Routes/product.js
+++ Routes/product.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read } = require ('../controllers/product');
+const { create, productById, read, remove } = require ('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 
 router.get('/product/:productId', read)
 router.post('/product/create/:userId', requireSignin,isAdmin,isAuth,create );
-
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin,remove)
 
 
 router.param("userId", userById);
--- controllers/product.js
+++ controllers/product.js
@@ -68,6 +68,21 @@ exports.create = (req,res) =>{
                 })
             }
             res.json(result)
+        });
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product
+        product.remove((err, deletedProduct)=>{
+            if(err){
+                return res.status(400).json({
+                    error:errorHandler(err)
+                });
+            }
+            res.json({
+                //deletedProduct,
+                "message":"Product deleted successfully"
+            })
         })
-    })
 }
